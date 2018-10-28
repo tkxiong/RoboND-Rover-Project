c9ea8e2d5f6073f8efc47e67fda9495840c25da9
kai xiong <legendarykx.kaixiong1@gmail.com>
2018-10-29 00:09:54 +0800

    Update perception.py, added find_rock_thresh func

--- code/perception.py
+++ code/perception.py
@@ -17,6 +17,22 @@ def find_path_thresh(img, rgb_thresh=(160, 140, 140)):
     # Return the binary image
     return color_select
 
+# Identify pixels above the threshold
+# Find rock pixel
+def find_rock_thresh(img, rgb_thresh=(100, 100, 40)):
+    # Create an array of zeros same xy size as img, but single channel
+    color_select = np.zeros_like(img[:,:,0])
+    # Require that each pixel be above all three threshold values in RGB
+    # above_thresh will now contain a boolean array with "True"
+    # where threshold was met
+    above_thresh = (img[:,:,0] > rgb_thresh[0]) \
+                & (img[:,:,1] > rgb_thresh[1]) \
+                & (img[:,:,2] < rgb_thresh[2])
+    # Index the array of zeros with the boolean array and set to 1
+    color_select[above_thresh] = 1
+    # Return the binary image
+    return color_select
+
 # Define a function to convert from image coords to rover coords
 def rover_coords(binary_img):
     # Identify nonzero pixels
